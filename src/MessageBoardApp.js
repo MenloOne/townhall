bb5efe174d8572664fc3091064e87aa3a42cd3bf
Pat Maddox <pmaddox@8thlight.com>
2018-02-21 09:07:30 -0700

    Remove unused code

--- src/MessageBoardApp.js
+++ src/MessageBoardApp.js
@@ -3,12 +3,6 @@ class MessageBoardApp {
     this.view = props.view;
     this.menloStorage = props.menloStorage;
 
-    this.messages = [
-      { id: 1, body: "App message 1" },
-      { id: 2, body: "App message 2" },
-      { id: 3, body: "App message 3" },
-    ];
-
     this.view.setState({
       onCreateMessage: this.createMessage.bind(this)
     });
