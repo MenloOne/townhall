4c05e4a57ad8159aa035a10bb5953aa3a7e1661c
Pat Maddox <pmaddox@8thlight.com>
2018-02-23 09:57:41 -0700

    Reduce duplication in `MessageBoardApp` test

--- src/MessageBoardApp.test.js
+++ src/MessageBoardApp.test.js
@@ -3,13 +3,17 @@ import MessageBoardApp from './MessageBoardApp';
 describe(MessageBoardApp, () => {
   describe('createMessage(messageBody)', () => {
     describe('successful', () => {
-      it('creates the message on local storage', done => {
-        let view = {setState: jest.fn()};
-        let localStorage = {createMessage: jest.fn(() => Promise.resolve('localHash'))};
-        let contract = {createMessage: jest.fn(() => Promise.resolve(true))};
-        let menloStorage = {createMessage: jest.fn(() => Promise.resolve('localHash'))};
-        let app = new MessageBoardApp({view: view, localStorage: localStorage, menloStorage: menloStorage, contract: contract});
+      let view, localStorage, contract, menloStorage, app;
 
+      beforeEach(() => {
+        view = {setState: jest.fn()};
+        localStorage = {createMessage: jest.fn(() => Promise.resolve('localHash'))};
+        contract = {createMessage: jest.fn(() => Promise.resolve(true))};
+        menloStorage = {createMessage: jest.fn(() => Promise.resolve('localHash')), messages: ['message 1', 'message 2']};
+        app = new MessageBoardApp({view: view, localStorage: localStorage, menloStorage: menloStorage, contract: contract});
+      });
+
+      it('creates the message on local storage', done => {
         app.createMessage("test message").then(() => {
           expect(localStorage.createMessage).toHaveBeenCalledWith({
             version: "CONTRACT_VERSION",
@@ -21,12 +25,6 @@ describe(MessageBoardApp, () => {
       });
 
       it('creates the message on the contract using the IPFS hash', done => {
-        let view = {setState: jest.fn()};
-        let localStorage = {createMessage: jest.fn(() => Promise.resolve('localHash'))};
-        let contract = {createMessage: jest.fn(() => Promise.resolve(true))};
-        let menloStorage = {createMessage: jest.fn(() => Promise.resolve('localHash'))};
-        let app = new MessageBoardApp({view: view, localStorage: localStorage, menloStorage: menloStorage, contract: contract});
-
         app.createMessage("test message").then(() => {
           expect(contract.createMessage).toHaveBeenCalledWith('localHash');
           done();
@@ -34,12 +32,6 @@ describe(MessageBoardApp, () => {
       });
 
       it('creates the message on menlo storage', done => {
-        let view = {setState: jest.fn()};
-        let localStorage = {createMessage: jest.fn(() => Promise.resolve('localHash'))};
-        let contract = {createMessage: jest.fn(() => Promise.resolve(true))};
-        let menloStorage = {createMessage: jest.fn(() => Promise.resolve('localHash'))};
-        let app = new MessageBoardApp({view: view, localStorage: localStorage, menloStorage: menloStorage, contract: contract});
-
         app.createMessage("test message").then(() => {
           expect(menloStorage.createMessage).toHaveBeenCalledWith({
             version: "CONTRACT_VERSION",
@@ -51,12 +43,6 @@ describe(MessageBoardApp, () => {
       });
 
       it('refreshes the messages view', done => {
-        let view = {setState: jest.fn()};
-        let localStorage = {createMessage: jest.fn(() => Promise.resolve('localHash'))};
-        let contract = {createMessage: jest.fn(() => Promise.resolve(true))};
-        let menloStorage = {createMessage: jest.fn(() => Promise.resolve('localHash')), messages: ['message 1', 'message 2']};
-        let app = new MessageBoardApp({view: view, localStorage: localStorage, menloStorage: menloStorage, contract: contract});
-
         app.createMessage("test message").then(() => {
           expect(view.setState).toHaveBeenLastCalledWith({messages: ['message 1', 'message 2']});
           done();
