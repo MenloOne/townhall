8c00aabc50108db1bc842424a872bcc50cf7f80f
Pat Maddox <pmaddox@8thlight.com>
2018-02-23 13:14:52 -0700

    Don't need to log in contract anymore

--- src/contracts/MemoryContract.js
+++ src/contracts/MemoryContract.js
@@ -1,7 +1,6 @@
 class MemoryContract {
   createMessage = (hash) => {
     return new Promise((resolve, reject) => {
-      console.log("Creating a message in contract!");
       resolve(true);
     });
   }
