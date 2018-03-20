1896db5026718d30fa8ecdac1f9d05a771e7f8c2
gustin <gustin@entryway.net>
2018-03-20 17:03:18 -0400

    Development network setup for Ganache as default, cli network for other clients like ganache-cli or parity dev.

--- truffle.js
+++ truffle.js
@@ -1,4 +1,21 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+  migrations_directory: "./migrations",
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    }
+    cli: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 500
+    }
+  }
+}
