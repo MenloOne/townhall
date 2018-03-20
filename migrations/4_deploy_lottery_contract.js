bbfdc87283bd8e6511e875326df4f9aeb7cf6f9c
Pat Maddox <pmaddox@8thlight.com>
2018-03-20 14:00:05 -0500

    Install the lottery app from two separate accounts

--- migrations/4_deploy_lottery_contract.js
+++ migrations/4_deploy_lottery_contract.js
@@ -9,5 +9,12 @@ module.exports = deployer => {
   Forum.deployed().then(instance => forum = instance);
   deployer.deploy(Lottery, token_address, forum_address)
     .then(() => Lottery.deployed())
-    .then(lottery => forum.setBeneficiary(lottery.address))
+    .then(lottery => {
+      forum.setBeneficiary(lottery.address);
+      AppToken.deployed()
+      .then(t => {
+        t.install(lottery.address, {from: '0xf17f52151ebef6c7334fad080c5704d77216b732'})
+        t.install(lottery.address, {from: '0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef'})
+      })
+    })
 }
