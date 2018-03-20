1598a5a60ab5a4882295cc9ee772a232dde875c2
Pat Maddox <pmaddox@8thlight.com>
2018-03-20 10:29:14 -0500

    Move app startup into window load event

--- src/index.js
+++ src/index.js
@@ -9,21 +9,22 @@ import MessageBoardGraph from 'storage/MessageBoardGraph';
 import MessageBoardView from 'components/MessageBoardView';
 import registerServiceWorker from './registerServiceWorker';
 
-let menloStorage = new RemoteIPFSStorage();
-let localStorage = new JavascriptIPFSStorage();
-localStorage.connectPeer(menloStorage);
-let contract = new MemoryContract();
-let graph = new MessageBoardGraph();
-let view = ReactDOM.render(<MessageBoardView />, document.getElementById('root'));
-let app = new MessageBoardApp({
-  view: view,
-  localStorage: localStorage,
-  graph: graph,
-  menloStorage: menloStorage,
-  contract: contract,
+window.addEventListener('load', function() {
+  let menloStorage = new RemoteIPFSStorage();
+  let localStorage = new JavascriptIPFSStorage();
+  localStorage.connectPeer(menloStorage);
+  let contract = new MemoryContract();
+  let graph = new MessageBoardGraph();
+  let view = ReactDOM.render(<MessageBoardView />, document.getElementById('root'));
+  let app = new MessageBoardApp({
+    view: view,
+    localStorage: localStorage,
+    graph: graph,
+    menloStorage: menloStorage,
+    contract: contract,
+  });
+
+  app.viewMessages();
+
+  registerServiceWorker();
 });
-
-app.viewMessages();
-window.app = app;
-
-registerServiceWorker();
