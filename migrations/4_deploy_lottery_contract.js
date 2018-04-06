bf6a94c0a194fbedf58a7e247d9888600ff952d7
gustin <gustin@entryway.net>
2018-04-06 11:17:02 -0400

    Use environments to populate accounts for contracts.

--- migrations/4_deploy_lottery_contract.js
+++ migrations/4_deploy_lottery_contract.js
@@ -12,16 +12,9 @@ module.exports = (deployer, network) => {
     trustee2 = '0xf17f52151ebef6c7334fad080c5704d77216b732';
     trustee3 = '0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef';
   } else {
-    /*
-    trustee1 = ENV['MENLO_TRUSTEE_1'];
-    trustee2 = ENV['MENLO_TRUSTEE_2'];
-    trustee3 = ENV['MENLO_TRUSTEE_3'];
-    personal = ENV['MENLO_ACCOUNT'];
-    */
-    // Parity Local
-    trustee1 = '0x008FfF80135Ac36aE8206E574e312FE8E64C34b2'
-    trustee2 = '0x0012c389c4E2557a29BC3988C3fA3E78d267376B'
-    trustee3 = '0x00F2f2d765E4f6d59FD43b63Fbe4035B1E6BBa02'
+    require('dotenv').config()
+    trustee1 = process.env.MENLO_TENET_1
+    trustee2 = process.env.MENLO_TENET_2
   }
 
   let forum;
--- migrations/5_personal_account.js
+++ migrations/5_personal_account.js
@@ -9,18 +9,14 @@ module.exports = (deployer, network) => {
     // candy maple cake sugar pudding cream honey rich smooth crumble sweet treat
     personal = '0x0d1d4e623d10f9fba5db95830f7d3839406c6af2';
   } else {
-    //personal = ENV['MENLO_ACCOUNT'];
-    personal = '0x0d1d4e623d10f9fba5db95830f7d3839406c6af2';
+    require('dotenv').config()
+    personal = process.env.MENLO_POSTER
   }
 
-  var forum_address;
-  Forum.deployed().then(instance => forum_address = instance.address);
-
   // Fund personal account with MET
+  let amount = 10000000000000000000000;
   AppToken.deployed().then(token => {
-    let amount = 10000000000000000000000000000;
     token.transfer(personal, amount);
-    token.approve(forum_address, amount,
-                {from: '0x0d1d4e623d10f9fba5db95830f7d3839406c6af2'});
+    token.approve(Forum.address, amount, {from: personal});
   })
 }
