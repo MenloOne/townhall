afa1edb21215d5da04a7f8284a90fab8feb6487a
gustin <gustin@entryway.net>
2018-04-09 08:46:49 -0400

    Refine creation of integration accounts, fund with ether.

--- scripts/create-accounts.js
+++ scripts/create-accounts.js
@@ -2,7 +2,7 @@ let Web3 = require('web3')
 
 console.log('Creating accounts')
 
-let provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545")
+let provider = new Web3.providers.HttpProvider("http://localhost:8545")
 let web3 = new Web3(provider)
 
 console.log("Creating tenents")
@@ -12,4 +12,26 @@ let trustee2 = web3.eth.accounts.create();
 let trustee3 = web3.eth.accounts.create();
 let poster = web3.eth.accounts.create();
 
-console.log(poster['address'])
+let geth_dev = ''
+let parity_dev = '0x00a329c0648769a73afac7f9381e08fb43dbea72'
+/*
+
+var tx = web3.eth.sendTransaction({from: parity_dev,
+                                     to: poster.address,
+                                     gas: 210000,
+                                     value: (10037037)})
+.then(function(receipt){
+  console.log(receipt)
+});
+console.log(poster)
+*/
+web3.eth.personal.newAccount('')
+.then(account => {
+  var tx = web3.eth.sendTransaction({from: parity_dev,
+                                      to: account,
+                                      gas: 210000,
+                                      value: (10037037)})
+  .then(function(receipt){
+    console.log(receipt)
+  })
+})
