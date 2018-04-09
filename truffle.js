005e76e1f04e923c6b2af47b158535fe7a533d10
gustin <gustin@entryway.net>
2018-04-09 18:27:34 -0400

    Remove explicit account to start truffle console from.

--- truffle.js
+++ truffle.js
@@ -4,7 +4,6 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 9545,
-      from: '0x0d1d4e623d10f9fba5db95830f7d3839406c6af2',
       network_id: "*"
     },
     ganache: {
