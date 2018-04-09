29a92455d1ade6757cd25abe0defaf8e4c29d858
gustin <gustin@entryway.net>
2018-04-09 08:44:03 -0400

    Base menlo setup and ipfs setup and config.

---
+++ scripts/menlo-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Check to see if Homebrew is installed, and install it if it is not
+command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew Now"; \
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
+
+./scripts/install-ipfs.sh
