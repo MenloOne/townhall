c5a6c10ac9507c5aadac23cc3afcfc32c5fc853e
matthewnolan <matthewnolan415@gmail.com>
2018-05-28 03:07:18 -0400

    readme

--- contracts/Forum.sol
+++ contracts/Forum.sol
@@ -1,5 +1,5 @@
 /*
-  Copyright 2018 Vulcanize, Inc.
+  Copyright 2018 Menlo, Inc.
 
   Licensed under the Apache License, Version 2.0 (the “License”);
   you may not use this file except in compliance with the License.
